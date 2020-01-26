ee9f067eea2f874c3cbcbcad01e871187cdcbbc6
sasha <dub1702@gmail.com>
2020-01-26 16:19:15 +0300

    trim

--- src/Classes/XMLWorker.java
+++ src/Classes/XMLWorker.java
@@ -15,8 +15,6 @@ import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public  class XMLWorker {
-
-
     public static ArrayList<FileObj> parseFiles() {
         ArrayList<FileObj> list = new ArrayList<FileObj>();
         try {
@@ -37,7 +35,6 @@ public  class XMLWorker {
         }
         return list;
     }
-
     public static ArrayList<UserObj> parseUsers() {
         ArrayList<UserObj> list = new ArrayList<UserObj>();
         try {
@@ -45,11 +42,8 @@ public  class XMLWorker {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(fXmlFile);
-
             doc.getDocumentElement().normalize();
-
             NodeList nList = doc.getElementsByTagName("user");
-
             for (int i = 0; i < nList.getLength(); i++) {
                 Element nNode = (Element) nList.item(i);
                 String userName = nNode.getAttribute("username");
@@ -58,7 +52,6 @@ public  class XMLWorker {
                 UserObj user = new UserObj(userName,userPass,userGrant);
                 list.add(user);
             }
-
         } catch (Exception ignored) {
         }
         return list;
@@ -66,70 +59,55 @@ public  class XMLWorker {
 
 
     public static void resetRights() throws TransformerException, ParserConfigurationException, FileNotFoundException, ParserConfigurationException {
-
         ArrayList<FileObj> files = parseFiles();
         ArrayList<UserObj> users = parseUsers();
-        Boolean value = false;
-
+        boolean value = false;
 
         // здесь создаем документ
+
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder build = factory.newDocumentBuilder();
         Document doc = build.newDocument();
-
         Element rootElement = doc.createElement("files");
-
         doc.appendChild(rootElement);
-
         for (int i = 0; i < files.size(); i++) {
             Element file = doc.createElement("file");
             file.setAttribute("filename", files.get(i).getName());
             rootElement.appendChild(file);
             for (int j = 0; j < users.size(); j++) {
-
                 Element user = doc.createElement("user");
                 user.setAttribute("username",users.get(j).getName());
-
                 value = rand(users.get(j).getGrant());
-                user.setAttribute("full", value.toString());
+                user.setAttribute("full", Boolean.toString(value));
                 if (!value){ value = rand(users.get(j).getGrant());}
-                user.setAttribute("write", value.toString());
+                user.setAttribute("write", Boolean.toString(value));
                 if (!value) {value = rand(users.get(j).getGrant());}
-                user.setAttribute("read", value.toString());
+                user.setAttribute("read", Boolean.toString(value));
                 if (!value) {value = rand(users.get(j).getGrant());}
-                user.setAttribute("transfer", value.toString());
+                user.setAttribute("transfer", Boolean.toString(value));
                 file.appendChild(user);
-
             }
         }
-
         Transformer t = TransformerFactory.newInstance().newTransformer();
         t.setOutputProperty(OutputKeys.INDENT, "yes");
         t.transform(new DOMSource(doc), new StreamResult(new FileOutputStream("usersRights.xml")));
     }
 
     public static ArrayList<FileObj> parseRights() {
-
         ArrayList<UserObj> users = new ArrayList<>();
         ArrayList<FileObj> files = new ArrayList<>();
         try {
-
             File fXmlFile = new File("usersRights.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(fXmlFile);
-
             doc.getDocumentElement().normalize();
-
             NodeList nFileList = doc.getElementsByTagName("file");
             for (int i = 0; i < nFileList.getLength(); i++) {
                 Element userList = (Element) nFileList.item(i);
-
-
                 String filename = userList.getAttribute("filename");
                 users = new ArrayList<>();
                 for (int j = 0; j < userList.getElementsByTagName("user").getLength(); j++) {
-
                     Element user = (Element) userList.getElementsByTagName("user").item(j);
                     String username = user.getAttribute("username");
                     String write = user.getAttribute("write");
@@ -139,7 +117,6 @@ public  class XMLWorker {
                     UserObj userObj = new UserObj(username,write,read,transfer,full);
                     users.add(userObj);
                 }
-
                 FileObj fileObj = new FileObj(filename,users);
                 files.add(fileObj);
             }
@@ -151,19 +128,14 @@ public  class XMLWorker {
 
 
     public static void applyRights(ArrayList<FileObj> files) throws TransformerException, FileNotFoundException, ParserConfigurationException {
-
         String value = "";
-
 
         // здесь создаем документ
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder build = factory.newDocumentBuilder();
         Document doc = build.newDocument();
-
         Element rootElement = doc.createElement("files");
-
         doc.appendChild(rootElement);
-
         for (int i = 0; i < files.size(); i++) {
             Element file = doc.createElement("file");
             file.setAttribute("filename", files.get(i).getName());
@@ -183,27 +155,20 @@ public  class XMLWorker {
                 file.appendChild(user);
             }
         }
-
         Transformer t = TransformerFactory.newInstance().newTransformer();
         t.setOutputProperty(OutputKeys.INDENT, "yes");
         t.transform(new DOMSource(doc), new StreamResult(new FileOutputStream("usersRights.xml")));
-
     }
 
 
     public static void addUser(ArrayList<UserObj> users) throws ParserConfigurationException, TransformerException, FileNotFoundException {
-
         String value = "";
-
         // здесь создаем документ
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder build = factory.newDocumentBuilder();
         Document doc = build.newDocument();
-
         Element rootElement = doc.createElement("users");
-
         doc.appendChild(rootElement);
-
         for (int i = 0; i < users.size(); i++) {
             Element user = doc.createElement("user");
             user.setAttribute("username",users.get(i).getName());
@@ -211,13 +176,10 @@ public  class XMLWorker {
             user.setAttribute("grant",users.get(i).getGrant());
             rootElement.appendChild(user);
         }
-
         Transformer t = TransformerFactory.newInstance().newTransformer();
         t.setOutputProperty(OutputKeys.INDENT, "yes");
         t.transform(new DOMSource(doc), new StreamResult(new FileOutputStream("Users.xml")));
-
     }
-
     private static boolean rand(String str){
         double a = 0;
         boolean newValue = false;
@@ -226,5 +188,4 @@ public  class XMLWorker {
         newValue = (str.equals("admin")) || newValue;
         return newValue;
     };
-
 }
